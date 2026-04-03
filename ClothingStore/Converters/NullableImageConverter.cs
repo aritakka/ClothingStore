2af08e4a4c5877c283bd2fee3b30f891f6de28c6
aritakka <normymagic1@gmail>
2026-04-03 12:29:34 +0300

    1

---
+++ ClothingStore/Converters/NullableImageConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ClothingStore.Converters
+{
+    public class NullableImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null; // или вернуть ImageSource по умолчанию, если есть
+
+            try
+            {
+                string path = value.ToString();
+                if (string.IsNullOrWhiteSpace(path))
+                    return null;
+
+                return new BitmapImage(new Uri(path, UriKind.RelativeOrAbsolute));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
